4117bd110d693e67a0830c15e6831a909592e32c
Vladimir Petrigo <vladimir.petrigo@gmail.com>
2022-11-30 12:25:23 +0300

    smf: Fix Clang pedantic errors related to IS_ENABLED macro

--- include/smf.h
+++ include/smf.h
@@ -22,13 +22,13 @@
  * Getting something that works in C and CPP for an arg that may or may
  * not be defined is tricky.  Here, if we have "#define CONFIG_BOOGER 1"
  * we match on the placeholder define, insert the "0," for arg1 and generate
- * the triplet (0, 1, 0).  Then the last step cherry picks the 2nd arg (a one).
+ * the triplet (0, 1, 0).  Then the last step cherry-picks the 2nd arg (a one).
  * When CONFIG_BOOGER is not defined, we generate a (... 1, 0) pair, and when
- * the last step cherry picks the 2nd arg, we get a zero.
+ * the last step cherry-picks the 2nd arg, we get a zero.
  */
 #define __is_defined(x) ___is_defined(x)
 #define ___is_defined(val) ____is_defined(__ARG_PLACEHOLDER_##val)
-#define ____is_defined(arg1_or_junk) __take_second_arg(arg1_or_junk 1, 0)
+#define ____is_defined(arg1_or_junk) __take_second_arg(arg1_or_junk 1, 0, 0)
 
 #define IS_BUILTIN(option) __is_defined(option)
 #define IS_ENABLED(option) IS_BUILTIN(option)
